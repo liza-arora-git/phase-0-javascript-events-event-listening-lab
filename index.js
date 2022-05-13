8098095d2278580d5b0826fcad108c01ba2d4a76
Liza Arora <liza.arora@outlook.com>
2022-05-14 03:05:17 +1000

    Lab Complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    const test = document.getElementById('fifth');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+    test.addEventListener('click', clickAlert);
 }
+addingEventListener();
